bf0de56da17913a76fe3fecdb16ff4caffd980e4
sterios <sterios.taskudis@gmail.com>
2019-09-26 20:36:24 +0300

    fix: change ImageLoader logic

--- src/scripts/ImageLoader.js
+++ src/scripts/ImageLoader.js
@@ -1,16 +1,22 @@
 import * as Pixi from 'pixi.js';
+import config from './utils/config';
 
 export default class ImageLoader extends Pixi.Loader {
-	constructor() {
-		super();
+	constructor(baseUrl = config.imagesBasePath) {
+		super(baseUrl);
 	}
 
 	loadAssets(assets) {
-		return new Promise((resolve) => {
-			this.add(assets);
-			this.load((loader, resources) => {
-				resolve(resources);
+			assets.forEach(element => {
+				this.add(element.name, element.fullName);
 			});
-		});
+
+			this.onError.add((e) => console.error('error', e));
+
+			return new Promise((resolve) => {
+				this.load((loader, resources) => {
+					resolve(resources);
+				});
+			});
 	}
 }
