26055d1c70c0de36b4e9c902e8939ba84f2abd60
sterios <sterios.taskudis@gmail.com>
2019-10-29 23:39:49 +0200

    fix: add some styles into the index.js file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,21 @@
 import App from './App';
 
-const game = new App();
-document.body.appendChild(game.view);
 
-game.init();
-game.preload();
+const start = async () => {
+  const game = new App();
+  document.body.appendChild(game.view);
+  document.body.style.background = 'black';
+  document.body.style.display = 'flex';
+  document.body.style.justifyContent = 'center';
+  document.body.style.alignItems = 'center';
+  document.body.style.height = '100vh';
+  document.body.style.overflow = 'hidden';
+  
+  game.init();
+  await game.preload();
+  game.createField();
+
+};
+
+
+start();
